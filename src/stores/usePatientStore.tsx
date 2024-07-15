8c7c09eda705429d5232cbee670629b11d60ce03
salvadoraita <salvadoraita@gmail.com>
2024-07-15 21:35:48 +0000

    20240715 17:35

--- src/stores/usePatientStore.tsx
+++ src/stores/usePatientStore.tsx
@@ -3,9 +3,9 @@ import { create } from "zustand"
 import { v7 as uuidv7 } from "uuid";
 import { ClsPatient } from "../classes/classes";
 
-
 type PatientState = {
     patients: ClsPatient[];
+    activePatient: ClsPatient | null;
     addPatient: (data: ClsPatient) => void;
     editPatient: (patient: ClsPatient) => void;
     deletePatient: (patient: ClsPatient) => void;
@@ -19,6 +19,7 @@ const createPatient = (data: ClsPatient): ClsPatient => {
 
 const usePatientStore = create<PatientState>( (set) => ({
     patients: [],
+    activePatient: null,
     addPatient: (data: ClsPatient) => {
         set( (state) => ({
             patients: [...state.patients, createPatient(data)]
@@ -28,7 +29,9 @@ const usePatientStore = create<PatientState>( (set) => ({
         console.log("edit", patient);
     },
     deletePatient: (patient: ClsPatient) => {
-        console.log("delete", patient);
+        set( (state) => ({
+            patients: state.patients.filter((p: ClsPatient) => p.id !== patient.id)
+        }))
     },
 }));
 
